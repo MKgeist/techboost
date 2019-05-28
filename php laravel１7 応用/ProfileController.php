b72b469e7dcd065bb6a583ed0ea1295db2e2fe8a
MKgeist <49156049+MKgeist@users.noreply.github.com>
2019-05-28 14:14:10 +0900

    php laravel１7 応用です。

---
+++ php laravel１7 応用/ProfileController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Profile;
+
+class ProfileController extends Controller
+{
+    //
+    public function add()
+{
+    return view('admin.profile.create');
+}
+
+public function create(Request $request)
+{
+  $this->validate($request, Profile::$rules);
+  $profile = new Profile;
+  $form = $request->all();
+  if ($form['image']){
+    $path = $request->file('image')->store('public/image');
+    $profile->image_path = basename($path);
+  } else {
+    $profile->image_path = null;
+  }
+  unset($form['_token']);
+  unset($form['image']);
+
+  $profile->fill($form);
+  $profile->save();
+
+  return redirect('admin/profile/create');
+}
+
+public function edit(Request $request)
+{
+  $profile = Profile::find($request->id);
+  if (empty($profile)) {
+  abort(404);
+}
+    return view('admin.profile.edit',['profile_form' => $profile]);
+}
+
+public function update(Request $request)
+{
+  $this->validate($request, profile::$rules);
+  // News Modelからデータを取得する
+  $profile = profile::find($request->id);
+  // 送信されてきたフォームデータを格納する
+  $profile_form = $request->all();
+  if (isset($profile_form['image'])) {
+    $path = $request->file('image')->store('public/image');
+    $profile->image_path = basename($path);
+    unset($news_form['image']);
+  } elseif (0 == strcmp($request->remove, 'true')) {
+    $profile->image_path = null;
+  }
+  unset($profile_form['_token']);
+  unset($profile_form['remove']);
+
+  // 該当するデータを上書きして保存する
+  $profile->fill($profile_form)->save();
+
+    return redirect('admin/profiles');
+}
+}
